905aa609ee8f101b1c514c699109656ebd6a0b80
mukizaisaac <123204114+mukizaisaac@users.noreply.github.com>
2023-04-10 18:36:45 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,11 @@
-#include "main.h"
 
+/**
+ * flip_bits - number of bits you would need to flip
+ * to get from one number to another
+ * @n: parameter
+ * @m: destiny
+ * Return: nflips
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, bflips = 0;
